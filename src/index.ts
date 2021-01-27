b96196f39af946b01b1403a99d39cd5ab28bbbc6
Dan Alexander <lxndrdagreat@gmail.com>
2021-01-26 23:17:30 -0500

    feat: adding dummy data generator

--- src/index.ts
+++ src/index.ts
@@ -46,3 +46,52 @@ export abstract class GoalStorageService {
 
   abstract getGoalCompletionScore(goal: GoalModel): Promise<GoalCompletionScore>;
 }
+
+export async function generateTestData(storageService: GoalStorageService): Promise<void> {
+  // Create some daily and weekly goals
+  const dailies: GoalUpsertData[] = [];
+  const weeklies: GoalUpsertData[] = [];
+  const titles = ['Alpha', 'Beta', 'Gamma', 'Delta', 'Epsilon', 'Zeta', 'Eta', 'Theta', 'Iota', 'Kappa', 'Lambda', 'Mu', 'Xi',];
+  for (let i = 0; i < 5; ++i) {
+    let date = new Date();
+    date.setFullYear(date.getFullYear() - 1);
+    date.setMonth(date.getMonth() + i);
+    dailies.push({
+      title: titles.pop() as string,
+      dateAdded: date,
+      occurs: GoalOccurrence.Daily
+    });
+    weeklies.push({
+      title: titles.pop() as string,
+      dateAdded: date,
+      occurs: GoalOccurrence.Weekly
+    });
+  }
+
+  const goals = await Promise.all([
+    ...dailies.map(daily => storageService.upsertGoal(daily)),
+    ...weeklies.map(weekly => storageService.upsertGoal(weekly))
+  ]);
+
+  // create daily completions
+  let count = 0;
+  for (const goal of goals.filter(goal => goal.occursDaily)) {
+    for (let i = 1; i < 14 - count; ++i) {
+      let date = new Date();
+      date.setDate(date.getDate() - i);
+      await storageService.toggleGoalCompletion(goal.id, date);
+    }
+    count += 2;
+  }
+
+  // create weekly completions
+  count = 0;
+  for (const goal of goals.filter(goal => goal.occursWeekly)) {
+    for (let i = 1; i < 6 - count; ++i) {
+      let date = new Date();
+      date.setDate(date.getDate() - (i * 7));
+      await storageService.toggleGoalCompletion(goal.id, date);
+    }
+    count += 1;
+  }
+}
